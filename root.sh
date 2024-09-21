fb6c56b55db12a566c9be6a2efd282f05befdcee
cedkeChat <123301168+00cedke@users.noreply.github.com>
2024-09-21 21:00:48 +0200

    Update root.sh

--- root.sh
+++ root.sh
@@ -5,12 +5,10 @@ PY_FILE="deployer.py"
 apt update && sudo apt upgrade -y
 
 apt install nginx -y
-
 systemctl start nginx
 systemctl enable nginx
 
 apt install certbot python3-certbot-nginx -y
-
 apt install php-fpm php-mysql -y
 
 apt install curl -y
@@ -21,6 +19,17 @@ apt install -y nodejs
 apt install python3 python3-pip -y
 
 apt install git -y
+
+apt install apt-transport-https ca-certificates curl software-properties-common -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+apt update
+apt install docker-ce -y
+systemctl start docker
+systemctl enable docker
+
+curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
 
 echo "nginx version:"
 nginx -v
@@ -42,6 +51,14 @@ pip3 --version
 
 echo "git version:"
 git --version
+
+echo "Docker version:"
+docker --version
+
+echo "Docker Compose version:"
+docker-compose --version
+
+usermod -aG docker $USER
 
 npm install -g docusaurus-init
 docusaurus-init
