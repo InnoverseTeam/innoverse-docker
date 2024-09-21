b6eb1e35597e25f09002c2f3963556840c4bbfa2
cedkeChat <123301168+00cedke@users.noreply.github.com>
2024-09-21 19:58:49 +0200

    Add files via upload

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -1,68 +1,149 @@
-const prismThemes = require("prism-react-renderer").themes;
+// @ts-check
+// `@type` JSDoc annotations allow editor autocompletion and type checking
+// (when paired with `@ts-check`).
+// There are various equivalent ways to declare your Docusaurus config.
+// See: https://docusaurus.io/docs/api/docusaurus-config
 
-const github = {
-    username: "InnoverseTeam",
-    repo: "innoverse-docker",
-};
+import {themes as prismThemes} from 'prism-react-renderer';
 
+/** @type {import('@docusaurus/types').Config} */
 const config = {
-    title: "Innoverse Docker Docs",
-    tagline: "An official Docker Compose setup for self-hosting an Innoverse server.",
-    favicon: "img/favicon.ico",
+  title: 'My Site',
+  tagline: 'Dinosaurs are cool',
+  favicon: 'img/favicon.ico',
 
-    // GitHub Pages deployment configuration
-    url: `https://${github.username.toLowerCase()}.github.io`,
-    baseUrl: `/${github.repo}/`,
-    organizationName: github.username,
-    projectName: github.repo,
+  // Set the production url of your site here
+  url: 'https://your-docusaurus-site.example.com',
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: '/',
 
-    onBrokenLinks: "throw",
-    onBrokenMarkdownLinks: "warn",
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'facebook', // Usually your GitHub org/user name.
+  projectName: 'docusaurus', // Usually your repo name.
 
-    i18n: {
-        defaultLocale: "en",
-        locales: ["en"],
-    },
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
 
-    presets: [
-        [
-            "classic",
-            {
-                docs: {
-                    routeBasePath: "/",
-                    sidebarPath: "./sidebars.js",
-                    editUrl: `https://github.com/${github.username}/${github.repo}/tree/main/docs/`,
-                },
-                blog: false,
-                pages: false,
-                theme: {
-                    customCss: "./css/main.css",
-                },
-            },
-        ],
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: './sidebars.js',
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        },
+        blog: {
+          showReadingTime: true,
+          feedOptions: {
+            type: ['rss', 'atom'],
+            xslt: true,
+          },
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          // Useful options to enforce blogging best practices
+          onInlineTags: 'warn',
+          onInlineAuthors: 'warn',
+          onUntruncatedBlogPosts: 'warn',
+        },
+        theme: {
+          customCss: './src/css/custom.css',
+        },
+      }),
     ],
+  ],
 
-    themeConfig: {
-        navbar: {
-            title: "Innoverse Docker Docs",
-            logo: {
-                alt: "Docusaurus Logo",
-                src: "img/logo.svg",
-            },
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      // Replace with your project's social card
+      image: 'img/docusaurus-social-card.jpg',
+      navbar: {
+        title: 'My Site',
+        logo: {
+          alt: 'My Site Logo',
+          src: 'img/logo.svg',
+        },
+        items: [
+          {
+            type: 'docSidebar',
+            sidebarId: 'tutorialSidebar',
+            position: 'left',
+            label: 'Tutorial',
+          },
+          {to: '/blog', label: 'Blog', position: 'left'},
+          {
+            href: 'https://github.com/facebook/docusaurus',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        links: [
+          {
+            title: 'Docs',
             items: [
-                {
-                    href: `https://github.com/${github.username}/${github.repo}`,
-                    label: "GitHub",
-                    position: "right",
-                },
+              {
+                label: 'Tutorial',
+                to: '/docs/intro',
+              },
             ],
-        },
-        prism: {
-            additionalLanguages: ["bash"],
-            theme: prismThemes.github,
-            darkTheme: prismThemes.dracula,
-        },
-    },
+          },
+          {
+            title: 'Community',
+            items: [
+              {
+                label: 'Stack Overflow',
+                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+              },
+              {
+                label: 'Discord',
+                href: 'https://discordapp.com/invite/docusaurus',
+              },
+              {
+                label: 'Twitter',
+                href: 'https://twitter.com/docusaurus',
+              },
+            ],
+          },
+          {
+            title: 'More',
+            items: [
+              {
+                label: 'Blog',
+                to: '/blog',
+              },
+              {
+                label: 'GitHub',
+                href: 'https://github.com/facebook/docusaurus',
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+      },
+      prism: {
+        theme: prismThemes.github,
+        darkTheme: prismThemes.dracula,
+      },
+    }),
 };
 
-module.exports = config;
+export default config;
